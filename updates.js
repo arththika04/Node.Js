f2094d5f58aa58502ae7200d69133b29385a87c5
Aarthika <arththika04@gmail.com>
2026-02-07 21:23:43 +0530

    first commit

--- updates.js
+++ updates.js
@@ -3,4 +3,6 @@ const fs =require("fs/Promises");
 async function updatefile() {
     let data = await fs.readfile("text.txt","utf8");
     data = data.replace("Hello", "Hi");
+    await fs.writeFile("test.txt", data);
+
 }
